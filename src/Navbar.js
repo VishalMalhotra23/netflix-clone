9e8538494a3cbd0a3adcc341b5c02935f4a6baec
Vishal Malhotra <vmalhotra362@gmail.com>
2022-05-05 21:29:44 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll", null);
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+        className="nav_logo"
+      />
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="Avatar"
+        className="nav_avatar"
+      />
+    </div>
+  );
+};
+
+export default Navbar;
